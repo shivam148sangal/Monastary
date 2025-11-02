dfe03f56496c52f5f7145c954af35a74a88f8e44
Shivam Sangal <shivamsangal94102@gmail.com>
2025-11-02 19:57:19 +0530

    Add login page integration

--- monarch/monarch/urls.py
+++ monarch/monarch/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('monastery.urls')),
+    path('accounts/', include('login.urls')),
 ]
